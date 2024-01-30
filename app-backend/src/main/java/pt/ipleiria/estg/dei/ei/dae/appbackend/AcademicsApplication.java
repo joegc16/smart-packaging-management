5165458d682e1dfa4eee36745093c02e8628c244
joegc16 <joeguillen16@gmail.com>
2024-01-30 17:49:34 +0000

    Backend App Initialization

---
+++ app-backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/appbackend/AcademicsApplication.java
@@ -0,0 +1,9 @@
+package pt.ipleiria.estg.dei.ei.dae.appbackend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AcademicsApplication extends Application {
+
+}
